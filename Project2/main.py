c69c14102bc1b52c4055e000618bece84dd38f72
nico0001 <nicolasjadoul0001@gmai.com>
2022-04-27 12:27:53 +0200

    Nearest neighbour...

--- Project2/main.py
+++ Project2/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import os
+
 def read(filepath):
     try:
         df = pd.DataFrame(columns=['number', 'id', 'time_sequence'])
@@ -10,11 +12,20 @@ def read(filepath):
         for i in range(5,len(lines)):
             line = lines[i].split(",")
             line = np.array(line).astype(np.float64)
-            matrix.append(list(line[3]+line[:3]))
+            matrix.append([line[3], *line[:3]])
         df.loc[0] = [number, id, matrix]
-        print(type(matrix))
         return df
     except IOError as e:
         print("Unable to read dataset file!\n")
+df = read("Sketch-Data-master\SketchData\SketchData\Domain01\833.txt")
 
-read("Project2\Sketch-Data-master\SketchData\SketchData\Domain01\833.txt").to_csv("Dataframe")
+directory = 'Sketch-Data-master\SketchData\SketchData\Domain01'
+ 
+# iterate over files in
+# that directory
+df = pd.DataFrame(columns=['number', 'id', 'time_sequence'])
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        df.append(read(f))
